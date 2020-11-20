a1fb0739a8c7d48ebfd55bf0f1d5877aa4e30dce
David Pesin <David.pesin@gmail.com>
2020-11-19 22:32:46 -0500

    Added EMPLOYEE, CONDUCTOR, and MECHANIC
    i think it's right but i could be wrong 😅

--- Databases.sql
+++ Databases.sql
@@ -70,6 +70,39 @@ CREATE TABLE EMPLOYEE(
   REPORTS_T0	FOREIGN ## FIX THIS W RECURSIVE SETUP##############################################################
   );
  
+-- Populating EMPLOYEE
+INSERT INTO `EMPLOYEE` (`EMP_ID`, `EMP_FNAME`, `EMP_LNAME`, `HIRE_DATE`, `JOB_CODE`, `REPORTS_TO`) VALUES
+(0001, 'Shea', 'Manser', '12/25/2016', 1, null), -- CEO
+(0010, 'Hesther', 'Collecott', '12/27/2013', 2, 0001), -- COO
+(0011, 'Crystie', 'Dowsey', '6/21/2006', 5, 0010), -- Station Manager 1
+(0100, 'Hulda', 'Geany', '11/2/2007', 5, 0010), -- Station Manager 2
+(0101, 'Gustaf', 'Wraith', '5/29/2012', 6, 0010), -- Train Manager 1
+(0110, 'Anya', 'Rusted', '2/22/2017', 6, 0010), -- Train Manager 2
+(0111, 'Skippy', 'Dodshun', '10/22/2005', 4, 0011), -- Head Mechanic 1
+(1000, 'Maia', 'Yuranovev', '7/26/2016', 4, 0100), -- Head Mechanic 2
+(1001, 'Pat', 'Britian', '2/10/2016', 9, 0111), --Mechanics
+(1010, 'Philipa', 'Bolesworth', '9/2/2002', 9, 0111),
+(1011, 'Ilyse', 'Krimmer', '4/19/2004', 9, 1000),
+(1100, 'Saraann', 'Hetterich', '10/5/2003', 9, 1000),
+(1101, 'Traci', 'Marciskewski', '8/4/2011', 11, 0011), -- Tellers
+(1110, 'Cobbie', 'Canfer', '4/22/2000', 11, 0100),
+(1111, 'Sloan', 'Quixley', '1/8/2001', 3, 0101), -- Head Steward 1
+(0002, 'Thornton', 'Pyke', '10/28/2004', 3, 0110), -- Head Steward 2
+(0020, 'Amity', 'Aidler', '6/5/2003', 7, 1111), -- Stewardesses 
+(0022, 'Karoly', 'Rippin', '9/26/2020', 7, 1111),
+(0200, 'Alecia', 'Filipowicz', '10/25/2020', 7, 0002),
+(0202, 'Rowan', 'Farnworth', '9/23/2002', 7, 0002),
+(0220, 'Sascha', 'Hallowes', '7/21/2002', 8, 1111), -- Cafe Attendants
+(0222, 'Darrel', 'Chiverstone', '7/10/2019', 8, 0002),
+(2000, 'Teodoor', 'Pottle', '4/25/2007', 5, 0010), --Station Manager 3
+(2002, 'Piggy', 'Cawte', '12/14/2019', 11, 2000), -- Teller
+(2020, 'Hale', 'Earengey', '6/4/2008', 4, 2000), -- Head Mechanic 3
+(2022, 'Cloe', 'Muddiman', '9/1/2013', 9, 2020), -- Mechanics (3)
+(2200, 'Camilla', 'Pitcher', '12/26/2009', 9, 2020),
+(2202, 'Vail', 'Cawt', '12/4/2000', 9, 2020),
+(2220, 'Bonnibelle', 'Bellam', '9/14/2020', 10, 0110), -- Conductors
+(2222, 'Dwain', 'Siviter', '12/20/2017', 10, 0101);
+ -- I'll remove the comments later bc we don't really need that, it's just for us
 CREATE TABLE STATION_ASSGN(
   STAT_ASSGN PRIMARY KEY
   FOREIGN KEY(EMP_ID) REFERENCES EMPLOYEE(EMP_ID)
@@ -144,9 +177,23 @@ CREATE TABLE MEHCANIC(
   CERT_EXP	VARCHAR(20) NOT NULL
   );
   
+-- Populating MECHANIC
+INSERT INTO `MECHANIC` (`EMP_ID`, `CERT_DATE`, `CERT_EXP`) VALUES
+(1001, '3/4/2019', '3/4/2024'),
+(1010, '12/5/2015', '12/5/2020'),
+(1011, '7/6/2016', '7/6/2021'),
+(1100, '7/6/2016', '7/6/2021'),
+(2022, '5/5/2017', '5/5/2022'),
+(2200, '8/5/2020', '8/5/2025'),
+(2202, '1/5/2018', '1/5/2023');
+
 CREATE TABLE CONDUCTOR
   FOREIGN KEY(EMP_ID) REFERENCES EMPLOYEE(EMP_ID)
   CERT_DATE	DATE NOT NULL
   CERT_EXP	DATE NOT NULL
   );
-  
+
+-- Populating CONDUCTOR
+INSERT INTO `CONDUCTOR` (`EMP_ID`, `CERT_DATE`, `CERT_EXP`) VALUES
+(2220, '1/8/2020', '1/8/2025'),
+(2222, '9/12/2019', '9/12/2024');
